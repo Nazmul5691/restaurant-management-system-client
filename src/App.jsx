d3e5aef3f7a229c8ba96125eea64d7cc638ba91f
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-04-20 23:52:53 +0600

    basic project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+      <h1 className='text-6xl text-red-500'>Vite + React</h1>
+      <h2 className='text-center'>hiiiii</h2>
+      
+    </>
+  )
+}
+
+export default App
