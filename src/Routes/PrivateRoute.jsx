563b31536938093f619681a2cbffde400f7fcf1d
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-04-25 21:22:06 +0600

    navigate user after successfully login

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -4,18 +4,22 @@ import { Navigate, useLocation } from "react-router-dom";
 
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext)
+
+    const {user, loading} = useContext(AuthContext);
     const location = useLocation();
 
     if(loading){
         return <span className="loading loading-spinner text-warning"></span>
     }
 
-    if (user) {
+    if(user){
         return children;
     }
 
     return <Navigate to='/login' state={{from: location}} replace />
+    
 };
 
 export default PrivateRoute;
+
+
