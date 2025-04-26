bb27c2853392b99bf91431960447fec763967ea9
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-04-27 00:20:54 +0600

    enforce-user-to-login-before-add-to-the-cart

--- src/shared/FoodCard.jsx
+++ src/shared/FoodCard.jsx
@@ -1,7 +1,34 @@
+import Swal from "sweetalert2";
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 
-const FoodCard = ({item}) => {
-    const {image, price, name,recipe} = item;
+const FoodCard = ({ item }) => {
+    const { image, price, name, recipe } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    const handleAddToCart = (food) => {
+        // console.log(food, user.email);
+        if (user && user.email) {
+            //
+        }
+        else {
+            Swal.fire({
+                title: "You are not login",
+                text: "Please login first to add to the cart !",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login !"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login')
+                }
+            });
+        }
+    }
 
     return (
         <div className="card bg-base-100 w-96 shadow-sm">
@@ -15,7 +42,7 @@ const FoodCard = ({item}) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn-outline border-[1px]   border-b-[3px] btn">Order Now</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn-outline border-[1px] border-b-[3px] btn">Order Now</button>
                 </div>
             </div>
         </div>
